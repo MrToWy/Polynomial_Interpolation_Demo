ceec82eb7199f938b02032affd5e0bc884d1114e
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2023-06-25 16:01:22 +0200

    add point

--- js/threeMain.js
+++ js/threeMain.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import { Line2 } from 'three/examples/jsm/lines/Line2.js';
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial.js';
 import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry.js';
+import {Mesh, MeshBasicMaterial, SphereGeometry} from "three";
 
 
 
@@ -20,6 +21,7 @@ document.body.appendChild( renderer.domElement );
 
 scene.add( getAxis(1., 0., 0xFFFFFF) ); // x-axis
 scene.add( getAxis(0., 1., 0xFFFFFF) ); // y-axis
+scene.add( getPoint(10.,10.));
 
 
 renderer.render( scene, camera );
@@ -30,32 +32,39 @@ function getAxis(x = 0., y = 1., color = 0xffff00, size = 25){
   dir.normalize();
 
   const origin = new THREE.Vector3( 0, 0, 0 );
-  let arrow = new THREE.ArrowHelper( dir, origin, size, color, 0.75, 0.5 );
 
-  let mat = new LineMaterial()
-  mat.linewidth = 0.009
-  mat.color.set(color)
-  mat.vertexColors = false
+  let mat = new LineMaterial();
+  mat.linewidth = 0.009;
+  mat.color.set(color);
+  mat.vertexColors = false;
 
-  let geometry = new LineGeometry()
+  let geometry = new LineGeometry();
   let points = [];
-  points.push(origin.x)
-  points.push(origin.y)
-  points.push(0.)
+  points.push(origin.x);
+  points.push(origin.y);
+  points.push(0.);
 
-  points.push(x * size)
-  points.push(y * size)
-  points.push(0.)
-  geometry.setPositions(points)
+  points.push(x * size);
+  points.push(y * size);
+  points.push(0.);
+  geometry.setPositions(points);
 
-  let line2 = new Line2()
-  line2.material = mat
-  line2.geometry = geometry
+  let line2 = new Line2();
+  line2.material = mat;
+  line2.geometry = geometry;
   line2.computeLineDistances();
   line2.scale.set( 1, 1, 1 );
 
-  // move center to (0,0)
-  line2.translateX(-size/2)
-
   return line2;
+}
+
+function getPoint(x,y,radius = 1., color = 0xffff00) {
+  let geo = new SphereGeometry(radius);
+  let mat = new MeshBasicMaterial({color: color});
+  let point = new Mesh(geo, mat);
+
+  point.translateX(x);
+  point.translateY(y);
+
+  return point;
 }
