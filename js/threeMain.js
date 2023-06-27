86c773bc3c971659f40a49280578c06dad52c1d2
Tobias Wylega <tobias@wylega.de>
2023-06-27 14:21:26 +0200

    add buttons

--- js/threeMain.js
+++ js/threeMain.js
@@ -4,34 +4,42 @@ import {getAxis, getPoints, getPolynom, getRungePoints, interpolate} from "./dra
 
 const startTime = Date();
 
-const scene = new THREE.Scene();
-const xAxisSize = 5;
-const yAxisSize = 5;
-const pointSize = 0.07;
-const interpolationStepSize = 0.01;
-const rungePointCount = 21;
+let rungePointCount = 21;
 
 
-const aspectRatio = 1; // window.innerWidth / window.innerHeight
-const camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 500);
-camera.position.set(0, 0, 14);
-camera.lookAt(0, 0, 0);
+function render(){
+  const scene = new THREE.Scene();
+  const xAxisSize = 5;
+  const yAxisSize = 5;
+  const pointSize = 0.07;
+  const interpolationStepSize = 0.01;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerHeight, window.innerHeight); // leave this quadratic, else linewidth will be weird
-renderer.setClearColor("#141C24");
-document.body.appendChild(renderer.domElement);
+  const aspectRatio = 1; // window.innerWidth / window.innerHeight
+  const camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 500);
+  camera.position.set(0, 0, 14);
+  camera.lookAt(0, 0, 0);
 
-scene.add(getAxis(-xAxisSize, -yAxisSize, 0x999999));
-scene.add(getAxis(xAxisSize, yAxisSize));
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerHeight, window.innerHeight); // leave this quadratic, else linewidth will be weird
+  renderer.setClearColor("#141C24");
+  document.body.appendChild(renderer.domElement);
 
-let points = getRungePoints(rungePointCount);
-let polynomArray = interpolate(points);
-scene.add(getPolynom(interpolationStepSize, xAxisSize, polynomArray));
-scene.add(getPoints(points, pointSize));
+  scene.add(getAxis(-xAxisSize, -yAxisSize, 0x999999));
+  scene.add(getAxis(xAxisSize, yAxisSize));
 
-renderer.render(scene, camera);
+  let points = getRungePoints(rungePointCount);
+  let polynomArray = interpolate(points);
+  scene.add(getPolynom(interpolationStepSize, xAxisSize, polynomArray));
+  scene.add(getPoints(points, pointSize));
 
-const endTime = Date();
-const deltaTime = endTime - startTime;
-console.log("Runtime: ", deltaTime)
+  renderer.render(scene, camera);
+
+  const endTime = Date();
+  const deltaTime = endTime - startTime;
+  console.log("Runtime: ", deltaTime)
+}
+
+export function setPointCount(count){
+  rungePointCount = count;
+  render()
+}
