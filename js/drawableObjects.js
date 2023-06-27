ad0edabefd8d115547c3cce9032a950d1f3e0fb7
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2023-06-27 15:39:00 +0200

    refactor

--- js/drawableObjects.js
+++ js/drawableObjects.js
@@ -2,7 +2,6 @@ import { Line2 } from "three/examples/jsm/lines/Line2.js";
 import { LineMaterial } from "three/examples/jsm/lines/LineMaterial.js";
 import { LineGeometry } from "three/examples/jsm/lines/LineGeometry.js";
 import {CircleGeometry, Group, Mesh, MeshBasicMaterial, Vector3} from "three";
-import * as mathjs from "mathjs";
 
 export function getAxis(xAxisSize = 5, yAxisSize = 5, color = 0xffffff, lineWidth = 0.0025) {
   let origin = new Vector3(0, 0, 0);
@@ -43,29 +42,6 @@ export function getLine(points, color = 0xffff00, lineWidth = 0.0065) {
   return line2;
 }
 
-export function interpolate(points) {
-  if (points.length <= 0) throw new Error("Bitte Punkte übergeben.");
-
-  let matrix = [];
-  let vector = [];
-
-  let degree = points.length - 1;
-
-  for (const point of points) {
-    let rowArray = [];
-
-    for (let n = 0; n <= degree; n++) {
-      rowArray.push(Math.pow(point.x, n));
-    }
-    matrix.push(rowArray);
-    vector.push(point.y);
-  }
-
-  let inverse = mathjs.inv(matrix);
-
-  return mathjs.multiply(inverse, vector);
-}
-
 function concatVector3Array(array) {
   let flatArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -82,41 +58,4 @@ export function getPoints(pointsArray, pointSize) {
     group.add(getPoint(point, pointSize));
   }
   return group;
-}
-
-function calcY(x, polynomArray) {
-  let result = 0;
-
-  for (let n = 0; n < polynomArray.length; n++) {
-    result += polynomArray[n] * Math.pow(x, n);
-  }
-
-  return result;
-}
-
-export function getPolynom(stepSize, xAxisSize, polynomArray) {
-  let points = [];
-  for (let x = -xAxisSize; x < xAxisSize; x += stepSize) {
-    points.push(new Vector3(x, calcY(x, polynomArray), 0));
-  }
-  return getLine(points, 0xff0000);
-}
-
-export function getRungePoints(pointCount) {
-  const points = [];
-
-    const degree = pointCount - 1;
-    const leftBoundary = -5;
-    const rightBoundary = 5;
-    const distance = (leftBoundary - rightBoundary) * -1;
-    const stepSize = distance / degree;
-
-    for (let i = 0; i < pointCount; i++) {
-      const x = leftBoundary + i * stepSize;
-      const y = 1. / (1. + Math.pow(x, 2.))
-      points.push(new Vector3(x, y, 0));
-    }
-
-    points.push();
-    return points;
 }
--- js/lineareInterpolation.js
+++ js/lineareInterpolation.js
@@ -1,6 +1,8 @@
 import "regenerator-runtime/runtime";
 import * as THREE from "three";
-import {getAxis, getPoints, getPolynom, getRungePoints, interpolate} from "./drawableObjects";
+import {getAxis, getLine, getPoints} from "./drawableObjects";
+import * as mathjs from "mathjs";
+import {Vector3} from "three";
 
 const startTime = Date();
 
@@ -50,4 +52,64 @@ function render(){
 export function setPointCount(count){
   rungePointCount = count;
   render()
+}
+
+export function interpolate(points) {
+  if (points.length <= 0) throw new Error("Bitte Punkte übergeben.");
+
+  let matrix = [];
+  let vector = [];
+
+  let degree = points.length - 1;
+
+  for (const point of points) {
+    let rowArray = [];
+
+    for (let n = 0; n <= degree; n++) {
+      rowArray.push(Math.pow(point.x, n));
+    }
+    matrix.push(rowArray);
+    vector.push(point.y);
+  }
+
+  let inverse = mathjs.inv(matrix);
+
+  return mathjs.multiply(inverse, vector);
+}
+
+export function getPolynom(stepSize, xAxisSize, polynomArray) {
+  let points = [];
+  for (let x = -xAxisSize; x < xAxisSize; x += stepSize) {
+    points.push(new Vector3(x, calcY(x, polynomArray), 0));
+  }
+  return getLine(points, 0xff0000);
+}
+
+export function getRungePoints(pointCount) {
+  const points = [];
+
+  const degree = pointCount - 1;
+  const leftBoundary = -5;
+  const rightBoundary = 5;
+  const distance = (leftBoundary - rightBoundary) * -1;
+  const stepSize = distance / degree;
+
+  for (let i = 0; i < pointCount; i++) {
+    const x = leftBoundary + i * stepSize;
+    const y = 1. / (1. + Math.pow(x, 2.))
+    points.push(new Vector3(x, y, 0));
+  }
+
+  points.push();
+  return points;
+}
+
+function calcY(x, polynomArray) {
+  let result = 0;
+
+  for (let n = 0; n < polynomArray.length; n++) {
+    result += polynomArray[n] * Math.pow(x, n);
+  }
+
+  return result;
 }
