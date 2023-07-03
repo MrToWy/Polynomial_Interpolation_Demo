1960361df43a09dc74f3b90c4cf56a0f156ca20b
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2023-07-04 00:01:55 +0200

    add card-layout css

--- modules/bezier/bezier.js
+++ modules/bezier/bezier.js
@@ -9,6 +9,8 @@ let bernsteinScene = new BernsteinScene("canvasRight").render();
 
 
 window.addEventListener( 'click', (e) => casteljauScene.onDocumentMouseDown(e, casteljauScene), false );
+document.getElementById('card1').addEventListener( 'click', () => document.getElementById("card1").classList.toggle('active'));
+document.getElementById('card2').addEventListener( 'click', () => document.getElementById("card2").classList.toggle('active'));
 document.getElementById("pause").addEventListener('click',e => {
   casteljauScene.togglePause(e, casteljauScene);
   bernsteinScene.togglePause(e, bernsteinScene);
