9e7d11f558e2bb88c286b1280f09a08c92b1d4ca
sophiefuerhoff <sophie.fuerhoff@gmx.net>
2023-07-04 11:22:04 +0200

    change eventlistener

--- modules/bezier/bezier.js
+++ modules/bezier/bezier.js
@@ -9,19 +9,19 @@ let bernsteinScene = new BernsteinScene("canvasRight").render();
 
 
 window.addEventListener( 'click', (e) => casteljauScene.onDocumentMouseDown(e, casteljauScene), false );
-document.getElementById('card1').addEventListener( 'click', () => {
+document.getElementById('upper-card1').addEventListener( 'click', () => {
   document.getElementById("card1").classList.toggle('active')
   casteljauScene.step = 1;
 });
-document.getElementById('card2').addEventListener( 'click', () => {
+document.getElementById('upper-card2').addEventListener( 'click', () => {
   document.getElementById("card2").classList.toggle('active')
   casteljauScene.step = 2;
 });
-document.getElementById('card3').addEventListener( 'click', () => {
+document.getElementById('upper-card3').addEventListener( 'click', () => {
   document.getElementById("card3").classList.toggle('active')
   casteljauScene.step = 3;
 });
-document.getElementById('card4').addEventListener( 'click', () => {
+document.getElementById('upper-card4').addEventListener( 'click', () => {
   document.getElementById("card4").classList.toggle('active')
   casteljauScene.step = 4;
 });
